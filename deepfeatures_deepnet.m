be93eceab19f9f4349b0464b8ddb94e87e062942
Niyazi <akniazi001@users.noreply.github.com>
2020-11-24 10:57:12 +0200

    Update deepfeatures_deepnet.m

--- deepfeatures_deepnet.m
+++ deepfeatures_deepnet.m
@@ -2,7 +2,7 @@
    net = resnet50();
 %  net = vgg16;;
 %  net = googlenet
-   analyzeNetwork(net);
+%    analyzeNetwork(net);
  
 net.Layers(1);
 inputSize = net.Layers(1).InputSize;
@@ -17,24 +17,15 @@ imageAugmenter = imageDataAugmenter( ...
 augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain, ...
     'DataAugmentation',imageAugmenter);
 % augimdsTest = augmentedImageDatastore(inputSize(1:2),imdsValidation);
-%% vgg16            
-%   layer1='pool2';
-%    layer2='pool3';
-%    layer3= 'fc8';
-%   
-%%googlenet
-%   layer1='pool3-3x3_s2';
-%    layer2='pool4-3x3_s2';
-%    layer3= 'loss3-classifier';
 
 %%  resnet
-layerneg = 'activation_29_relu';
+layerneg = 'activation_29_relu'; %% get features from particular layers
 layer0 = 'activation_35_relu';
 layer1 = 'activation_41_relu';
 layer2= 'activation_45_relu';
    layer3= 'fc1000';
-%    layer4= 'res5b_relu';
-Xng = activations(net,augimdsTrain,layerneg);
+ 
+Xng = activations(net,augimdsTrain,layerneg); %% Xng, X0,..,X3 are indicating the deep features vector of different layers of ResNet Model. 
  Xng= squeeze(mean(Xng,[1 2]))';
 X0 = activations(net,augimdsTrain,layer0);
  X0= squeeze(mean(X0,[1 2]))';
@@ -45,19 +36,5 @@ X0 = activations(net,augimdsTrain,layer0);
  X3 = activations(net,augimdsTrain,layer3,'OutputAs','rows');
 whos featuresTrain
 YTrain = imdsTrain.Labels;
-% Xng=0;
-% X0=0;
-% layer15 = 'activation_26_relu';
-% layer18 = 'activation_29_relu';
-% layer24 = 'activation_32_relu';
-% layerneg = 'activation_35_relu';
-% layer0 = 'activation_38_relu';
-% layer1 = 'activation_41_relu';
-% layer2= 'activation_45_relu';
-%    layer3= 'fc1000';
-% %    layer4= 'res5b_relu';
-% X15 = activations(net,augimdsTrain,layer15);
- 
- 
 
  end
