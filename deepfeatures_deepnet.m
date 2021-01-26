d203d783fa2b376aecc04d0312f2e65c63e245af
Altaf Khan Niazi <akniazi001@users.noreply.github.com>
2021-01-26 09:38:17 +0200

    Update deepfeatures_deepnet.m

--- deepfeatures_deepnet.m
+++ deepfeatures_deepnet.m
@@ -1,5 +1,5 @@
  function [YTrain,Xng,X0, X1,X2,X3]= deepfeatures_deepnet(imdsTrain )
-   net = resnet50();
+   net = resnet50(); %% only ResNet model is given here, you may use other CNN pre-trained model 
 %  net = vgg16;;
 %  net = googlenet
 %    analyzeNetwork(net);
@@ -18,8 +18,8 @@ augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain, ...
     'DataAugmentation',imageAugmenter);
 % augimdsTest = augmentedImageDatastore(inputSize(1:2),imdsValidation);
 
-%%  resnet
-layerneg = 'activation_29_relu'; %% get features from particular layers
+%%  resnet intermediate layers
+layerneg = 'activation_29_relu'; %% get features from particular intermediate layers
 layer0 = 'activation_35_relu';
 layer1 = 'activation_41_relu';
 layer2= 'activation_45_relu';
@@ -28,7 +28,7 @@ layer2= 'activation_45_relu';
 Xng = activations(net,augimdsTrain,layerneg); %% Xng, X0,..,X3 are indicating the deep features vector of different layers of ResNet Model. 
  Xng= squeeze(mean(Xng,[1 2]))';
 X0 = activations(net,augimdsTrain,layer0);
- X0= squeeze(mean(X0,[1 2]))';
+ X0= squeeze(mean(X0,[1 2]))'; % this function work as Average pooling for N number of images, NXL feature vectors return, L is a feature vector per image.
  X1 = activations(net,augimdsTrain,layer1);
  X1= squeeze(mean(X1,[1 2]))';
  X2 = activations(net,augimdsTrain,layer2);
@@ -36,5 +36,4 @@ X0 = activations(net,augimdsTrain,layer0);
  X3 = activations(net,augimdsTrain,layer3,'OutputAs','rows');
 whos featuresTrain
 YTrain = imdsTrain.Labels;
-
  end
